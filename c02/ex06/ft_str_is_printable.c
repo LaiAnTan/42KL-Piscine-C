d8bffbba3439939f5568e865ab3a3a263826259c
Tan Lai An <tlai-an@u82z05s02.42kl.edu.my>
2022-05-27 11:26:59 +0800

    edited c02 ex06

--- c02/ex06/ft_str_is_printable.c
+++ c02/ex06/ft_str_is_printable.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int ft_str_is_uppercase(char *str);
+int ft_str_is_printable(char *str);
 
-int ft_str_is_uppercase(char *str)
+int ft_str_is_printable(char *str)
 {
     int x;
 
@@ -22,7 +22,7 @@ int main()
 {   
     int value;
     char str1[50] = "DDDSKDSKDJ";
-    value = ft_str_is_uppercase(str1);
+    value = ft_str_is_printable(str1);
     printf("%d\n", value);
     return 0;
 }
