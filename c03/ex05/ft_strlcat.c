eb900a653a351b3da5c632aeaab3001c33ba7316
Lai An <tanlaian2004@gmail.com>
2022-05-31 00:03:43 +0800

    added c03 ex05

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -1,49 +1,56 @@
 #include <stdio.h>
+#include <string.h>
 
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size);
 
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
 {
-    int count1;
-    int count2;
-    
-    count1 = 0;
-    count2 = 0;
-    size = (size - 1);
-    if (size < 0)
+    int n;
+    int m;
+    int o;
+    int p;
+
+    n = -1;
+    m = 0;
+    o = -1;
+    p = -1;
+    while (src[++p] != '\0'){}
+    while (dest[++o] != '\0'){}
+    while (dest[++n] != '\0')
+        size--;
+    while (size != 0)
     {
-        return count1;
-    }
-    if (size > 0)
-    {
-        while(dest[count1] != '\0')
+        while (src[m] != '\0')
         {
-            count1++;
-        }
-        while(src[count2] != '\0')
-        {
-            if (dest[count1] == '\0')
+            if (dest[n] == '\0')
             {
-                dest[count1] = src[count2];
-                count1++;
-                count2++;
+                dest[n++] = src[m++];
+                size--;
+                if (size == 0)
+                    break;
             }
         }
-        dest[count1] = '\0';
-        return count1;
+        dest[n - 1] = '\0';
     }
-
+    return (o + p);
 }
 
 int main()
 {
     char str1[5] = "efgh";
-    char str2[8]= "abcd";
-    int e;
-    e = ft_strlcat(str2, str1, 6);
-    ft_strlcat(str2, str1, 6);
-    printf("%d\n", e);
-    printf("%s\n", str2);
+    char str2[8] = "abcd";
+    char str3[8] = "abcd";
+    int num1;
+    int num2;
+
+
+
+    num1 = ft_strlcat(str2, str1, 6);
+    num2 = strlcat(str3, str1, 6);
+    printf("My function = %s\n", str2);
+    printf("%d\n", num1);
+    printf("System function = %s\n", str3);
+    printf("%d\n", num2);
 
     return 0;
 }
