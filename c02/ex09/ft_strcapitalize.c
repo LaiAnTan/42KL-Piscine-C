a0921ccfab5521d27e96e9781903d5c6d52592d6
Tan Lai An <tlai-an@u82z05s02.42kl.edu.my>
2022-06-01 12:03:27 +0800

    fixed c02 ex09 & ex11 (ex10 broken)

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -1,78 +1,65 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcapitalizetest.c                             :+:      :+:    :+:   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tlai-an <tlai-an@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 17:19:18 by tlai-an           #+#    #+#             */
-/*   Updated: 2022/06/01 11:14:07 by tlai-an          ###   ########.fr       */
+/*   Updated: 2022/06/01 11:48:54 by tlai-an          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+char	*ft_strcaptialize(char *str);
+char	*lowcase(char *str);
+void	capital(char *str);
+int		checkletter(char *str);
+int		checknumber(char *str);
 
-#include <stdio.h>
-
-char *ft_strcaptialize(char *str);
-char *lowcase(char *str);
-void capital(char *str);
-int checkletter(char *str);
-int checknumber(char *str);
-
-char *ft_strcapitalize(char *str)
+char	*ft_strcapitalize(char *str)
 {
-    int count;
+	int	count;
 
-    count = -1;
-    lowcase(str);
-    while (str[++count] != '\0')
-    {
-        if (!(checkletter(&str[count - 1])) && (!checknumber(&str[count - 1])))
-            capital(&str[count]);
-    }
-    return (str);
+	count = -1;
+	lowcase(str);
+	while (str[++count] != '\0')
+	{
+		if (!(checkletter(&str[count - 1])) && (!checknumber(&str[count - 1])))
+			capital(&str[count]);
+	}
+	return (str);
 }
 
-char *lowcase(char *str)
+char	*lowcase(char *str)
 {
-    int n;
+	int	n;
 
-    n = -1;
-    while (str[++n] != '\0')
-    {
-        if(str[n] >= 'A' && str[n] <= 'Z')
-            str[n] += 32;
-    }
-    return (str);
+	n = -1;
+	while (str[++n] != '\0')
+	{
+		if (str[n] >= 'A' && str[n] <= 'Z')
+			str[n] += 32;
+	}
+	return (str);
 }
 
-void capital(char *str)
+void	capital(char *str)
 {
-   if(*str >= 'a' && *str <= 'z')
-        *str -= 32;
+	if (*str >= 'a' && *str <= 'z')
+		*str -= 32;
 }
 
-int checkletter(char *str)
+int	checkletter(char *str)
 {
-    if (*str < 'A' || *str > 'Z' && *str < 'a' || *str > 'z')
-        return (0);
-    else
-        return (1);
-    
+	if (*str < 'A' || (*str > 'Z' && *str < 'a') || *str > 'z')
+		return (0);
+	else
+		return (1);
 }
 
-int checknumber(char *str)
+int	checknumber(char *str)
 {
-    if (*str < '0' || *str > '9')
-        return (0);
-    else
-        return (1);
-}
-
-int main()
-{
-    char *e;
-    char arr[] = "TEST 123TEST";
-    e = ft_strcapitalize(arr);
-    printf("%s\n", e);
-    return (0);
+	if (*str < '0' || *str > '9')
+		return (0);
+	else
+		return (1);
 }
--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -6,34 +6,30 @@
 /*   By: tlai-an <tlai-an@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 17:19:28 by tlai-an           #+#    #+#             */
-/*   Updated: 2022/06/01 11:16:19 by tlai-an          ###   ########.fr       */
+/*   Updated: 2022/06/01 11:34:03 by tlai-an          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int n);
 
-#include <stdio.h>
-#include <string.h>
-
-unsigned int ft_strlcpy(char *dest, char *src, unsigned int n);
-
-unsigned int ft_strlcpy(char *dest, char *src, unsigned int n)
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int	i;
 
-    n = (n - 1);
-    i = 0;
-    if (n < 0)
-    {
-        return i;
-    }
-    if (n > 0)
-    {
-        while (src[i] != '\0' && n != 0)
-        {
-            dest[i]=src[i];
-            n--;
-            i++;
-        }
-    }
-    dest[i] = '\0';
-    return i;
+	n = (n - 1);
+	i = 0;
+	if (n < 0)
+	{
+		return (i);
+	}
+	if (n > 0)
+	{
+		while (src[i] != '\0' && n != 0)
+		{
+			dest[i] = src[i];
+			n--;
+			i++;
+		}
+	}
+	dest[i] = '\0';
+	return (i);
 }
