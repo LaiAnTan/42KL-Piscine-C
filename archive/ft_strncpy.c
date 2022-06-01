30cc8d074a61a865ad2ebe04f7dad177f992131e
Lai An <tanlaian2004@gmail.com>
2022-06-01 23:12:46 +0800

    1st june random changes

--- archive/ft_strncpy.c
+++ archive/ft_strncpy.c
@@ -5,27 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tlai-an <tlai-an@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/01 23:08:15 by tlai-an           #+#    #+#             */
-/*   Updated: 2022/06/01 23:10:50 by tlai-an          ###   ########.fr       */
+/*   Created: 2022/05/31 17:18:42 by tlai-an           #+#    #+#             */
+/*   Updated: 2022/06/01 15:01:53 by tlai-an          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n);
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int	i;
+	unsigned int	x;
 
-	i = 0;
-	while (i < n && src[i] != '\0')
+	x = 0;
+	while (n != x)
 	{
-		dest[i] = src[i];
-		i++;
+		dest[x] = src[x];
+		x++;
+		if (n == x)
+		{
+			break ;
+		}
 	}
-	while (i < n)
+	while (n > x)
 	{
-		dest[i] = '\0';
-		i++;
+		dest[x] = '\0';
+		x++;
 	}
 	return (dest);
 }
