db5f65ebefee7f6c5bcaa9a7bcb0ef8549bae281
Lai An <tanlaian2004@gmail.com>
2022-06-03 00:36:15 +0800

    added c06 ex01

---
+++ c06/ex01/ft_print_params.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int n;
+    int m;
+
+    n = 1;
+    m = 0;
+    while(n <= argc)
+    {
+        if (n > argc)
+            break;
+        while(argv[n][m] != '\0')
+            ++m;
+        write(1, argv[n], m);
+        write(1, "\n", 1);
+        ++n;
+    }
+    return (0);
+}
