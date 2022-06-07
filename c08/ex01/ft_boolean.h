7e437922be7ee0bbd5ecbea6934849b0e301434d
Lai An <tanlaian2004@gmail.com>
2022-06-08 00:19:12 +0800

    c08

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+typedef unsigned int t_bool;
+
+void	ft_putstr(char *str);
+t_bool	ft_is_even(int nbr);	
+
+#define TRUE 1
+#define FALSE 0
+#define EVEN_MSG "I have an even number of arguments.\n"
+#define ODD_MSG "I have an odd number of arguements.\n"
+#define SUCCESS 0
+#define EVEN(nbr) ((nbr) % (2) ? (TRUE) : (FALSE))
