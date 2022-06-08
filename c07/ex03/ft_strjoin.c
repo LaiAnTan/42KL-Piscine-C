81a60efca5c7646dd7f02a8f818b1da3e3749d57
Lai An <tanlaian2004@gmail.com>
2022-06-08 22:34:36 +0800

    c07

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -2,52 +2,36 @@
 #include <stdlib.h>
 
 char 	*ft_strjoin(int size, char **strs, char *sep);
-char	*ft_strdup(char *src);
 int		ft_strlen(char *src);
+char	*memalloc(int size, char **strs, char *sep);
+char	*ft_strcat(char *dest, char *src);
 
 char *ft_strjoin(int size, char **strs, char *sep)
 {
-	int	n;
+	int	strcnt;
+	int sepcnt;
 	int boolean;
 	char *dest;
 
-	n = 0;
+	strcnt = 0;
+	sepcnt = size - 1;
 	boolean = 0;
+	memalloc(size, strs, sep);
 	while (size--)
 	{
 		if(boolean == 0)
 		{
-			if((dest = (char*)malloc((ft_strlen(strs[n])) * sizeof(char)) == NULL))
-				return (0);
-			*dest = ft_strdup(strs[n]);
-			n++;
+			ft_strcat(dest, strs[strcnt]);
+			strcnt++;
 			boolean = 1;
 		}
 		if (boolean == 1)
 		{
-			if((dest = (char*)malloc((ft_strlen(sep)) * sizeof(char)) == NULL))
-				return (0);
-			*dest = ft_strdup(sep);
+			if (sepcnt--)
+				ft_strcat(dest, sep);
 			boolean = 0;
 		}
 	}
-
-}
-
-char	*ft_strdup(char *src)
-{
-	int n;
-	char *dest;
-
-	n = 0;
-	if((dest = (char*) malloc(n * sizeof(char))) == NULL)
-		return (0);
-	while (src[n])
-	{
-		dest[n] = src[n];
-		n++;
-	}
-	dest[n] = '\0';
 	return (dest);
 }
 
@@ -56,25 +40,63 @@ int	ft_strlen(char *str)
 	int	n;
 
 	n = 0;
-	while (str[n])
+	while (str[n] != '\0')
 		n++;
 	return (n);
 }
 
+char *memalloc(int size, char **strs, char *sep)
+{
+	int n;
+	int tlen;
+	char *dest;
+
+	n = 0;
+	tlen = 0;
+	while(size--)
+	{
+		tlen += ft_strlen(strs[n]);
+		n++;
+	}
+	size = n;
+	tlen += (ft_strlen(sep) * (size - 1));
+	if((dest = malloc(tlen * sizeof(char))) == 0)
+		return (0);
+	return (dest);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	return (dest);
+}
+
 int main()
 {
+	printf("\n");
 	char *test[] = 
 	{
-		"your mother",
-		"your mom",
-		"yomom",
-		"sex",
+		"segs",
+		"seggs",
+		"segggs",
+		"sexxxxxxx",
 	};
-	char sep[] = " ";
+	char sep[] = " i love ";
+	char *dest;
 	int size;
+	int total;
 
 	size = 4;
-	char *dest = *ft_strjoin(size, **test, *sep);
+	dest = ft_strjoin(size, test, sep);
 	printf("%s\n", dest);
 	return (0);
 }
